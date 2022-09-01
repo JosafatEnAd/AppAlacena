70f9a37cb1847d36864beedd25e9f5f534f5e206
Josafat <al222011221@gmail.com>
2022-09-01 00:09:31 -0500

    Inicio de repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [show, setShow]=useState(true);
+  const [Agregar, setAgregar]=useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className='App-body'>
+      <div className="App-header">
+        <div className="App-b">Bienvenid@</div>
+        <div className="App" onClick={() => setAgregar(!show)}>Agregar producto</div>
+        <div className="App">Editar producto</div>
+        <div className="App">Eliminar producto</div>
+      </div>
+      <button className="App-button" onClick={() => setShow(!show)}>{show? 'Mostrar Productos':'Ocultar Productos'}</button>
+      <div className="App-row-e">
+        <div className="App-column">
+          <div className="App-txt">Producto</div>
+          <div className="App-txt">Jamón</div>
+          <div className="App-txt">Jamón</div>
+          <div className="App-txt">Jamón</div>
+          <div className="App-txt">Jamón</div>
+          <div className="App-txt">Jamón</div>
+          <div className="App-txt">Jamón</div>
+          <div className="App-txt">Jamón</div>
+        </div>
+        <div className="App-column">
+          <div className="App-txt">Precio</div>
+          <div className="App-txt">$50</div>
+          <div className="App-txt">$50</div>
+          <div className="App-txt">$50</div>
+          <div className="App-txt">$50</div>
+          <div className="App-txt">$50</div>
+          <div className="App-txt">$50</div>
+        </div>
+        <div className="App-column">
+          <div className="App-txt">Cantidad</div>
+          <div className="App-txt">20</div>
+          <div className="App-txt">20</div>
+          <div className="App-txt">20</div>
+          <div className="App-txt">20</div>
+          <div className="App-txt">20</div>
+          <div className="App-txt">20</div>
+          <div className="App-txt">20</div>
+        </div>
+      </div>
+      <button className="App-button">Poca existencia</button>
+      <div className="App-row-pe">
+        <div className="App-column">
+          <div className="App-txt">Producto</div>
+          <div className="App-txt">Jamón</div>
+        </div>
+        <div className="App-column">
+          <div className="App-txt">Precio</div>
+          <div className="App-txt">$50</div>
+        </div>
+        <div className="App-column">
+          <div className="App-txt">Cantidad</div>
+          <div className="App-txt">20</div>
+        </div>
+      </div>
+      <button className="App-button">Sin existencia</button>
+      <div className="App-row-se">
+        <div className="App-column">
+          <div className="App-txt">Producto</div>
+          <div className="App-txt">Jamón</div>
+        </div>
+        <div className="App-column">
+          <div className="App-txt">Precio</div>
+          <div className="App-txt">$50</div>
+        </div>
+        <div className="App-column">
+          <div className="App-txt">Cantidad</div>
+          <div className="App-txt">20</div>
+        </div>
+      </div>
+    </body>
   );
 }
 
